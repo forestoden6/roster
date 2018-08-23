f7ecbb4a26c55f24ef7d2fbde0837a8bc8ef4cd0
forestoden6 <forestoden6@gmail.com>
2018-08-23 11:21:32 -0600

    Adding discovery client

--- src/main/java/io/rscale/training/TrainingUIApplication.java
+++ src/main/java/io/rscale/training/TrainingUIApplication.java
@@ -2,10 +2,12 @@ package io.rscale.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class TrainingUIApplication {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(TrainingUIApplication.class, args);
 	}
